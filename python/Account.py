7484a3fb0d674e0cb7f8bc8b4a6d9f991484f591
nferradas1998 <nbertoli32572@ucumberlands.edu>
2025-03-20 23:44:41 -0700

    Adding Python folder

---
+++ python/Account.py
@@ -0,0 +1,7 @@
+class Account:
+    
+    def __init__(self, description, category, amount, date_of_expense):
+        self.description = description
+        self.category = category
+        self.amount = amount
+        self.date = date_of_expense
